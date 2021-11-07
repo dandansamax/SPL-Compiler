9ab718c617a0f17f111bf85ed2f7b61f8753eac8
Zhao Yunlong <zao44457660@126.com>
2021-11-07 09:47:13 +0800

    add debug information

---
+++ src/utils/cmake-build-debug/CMakeFiles/SPL.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/d/Projects/CS323Compiler/SPL/hash_table.c" "/mnt/d/Projects/CS323Compiler/SPL/cmake-build-debug/CMakeFiles/SPL.dir/hash_table.c.o"
+  "/mnt/d/Projects/CS323Compiler/SPL/main.c" "/mnt/d/Projects/CS323Compiler/SPL/cmake-build-debug/CMakeFiles/SPL.dir/main.c.o"
+  "/mnt/d/Projects/CS323Compiler/SPL/symbol_table.c" "/mnt/d/Projects/CS323Compiler/SPL/cmake-build-debug/CMakeFiles/SPL.dir/symbol_table.c.o"
+  "/mnt/d/Projects/CS323Compiler/SPL/type.c" "/mnt/d/Projects/CS323Compiler/SPL/cmake-build-debug/CMakeFiles/SPL.dir/type.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/utils/cmake-build-debug/CMakeFiles/SPL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL.dir/hash_table.c.o"
+  "CMakeFiles/SPL.dir/main.c.o"
+  "CMakeFiles/SPL.dir/symbol_table.c.o"
+  "CMakeFiles/SPL.dir/type.c.o"
+  "SPL"
+  "SPL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SPL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
